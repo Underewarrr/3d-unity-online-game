8b26934da6b4397fd528f21da8f02fdf058a5b78
Rafhael Oliveira <underewar@gmail.com>
2022-10-01 16:46:42 -0300

    Dev: Player spawn object over to network

---
+++ Assets/Scripts/PlayerSpawnObject.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FishNet.Object;
+ 
+//Made by Bobsi Unity - Youtube
+public class PlayerSpawnObject : NetworkBehaviour
+{
+    public GameObject objToSpawn;
+    [HideInInspector] public GameObject spawnedObject;
+ 
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        if (!base.IsOwner)
+            GetComponent<PlayerSpawnObject>().enabled = false;
+    }
+ 
+    private void Update()
+    {
+        if(spawnedObject == null && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SpawnObject(objToSpawn, transform, this);
+        }
+ 
+        if (spawnedObject != null && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            DespawnObject(spawnedObject);
+        }
+    }
+ 
+    [ServerRpc]
+    public void SpawnObject(GameObject obj, Transform player, PlayerSpawnObject script)
+    {
+        GameObject spawned = Instantiate(obj, player.position + player.forward, Quaternion.identity); // Spawn Locally
+        ServerManager.Spawn(spawned); // Spawn on the Server
+        SetSpawnedObject(spawned, script); // set the object to spawn
+    }
+ 
+    [ObserversRpc]
+    public void SetSpawnedObject(GameObject spawned, PlayerSpawnObject script)
+    {
+        script.spawnedObject = spawned; // what object to spawn
+    }
+    // when despawning we send the object
+    [ServerRpc(RequireOwnership = false)]
+    public void DespawnObject(GameObject obj)
+    {
+        ServerManager.Despawn(obj);
+    }
+}
