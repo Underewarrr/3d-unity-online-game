9eb7032a2f212699262102dee25467de1592132b
Rafhael Oliveira <underewar@gmail.com>
2022-10-02 12:22:29 -0300

    Dev: Player inventory network

---
+++ Assets/Scripts/PlayerInvItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+[CreateAssetMenu(fileName = "PlayerInvItem", menuName = "Inventory/Item", order = 1)]
+public class PlayerInvItem : ScriptableObject
+{
+    public string itemName;
+    public GameObject prefab;
+}
